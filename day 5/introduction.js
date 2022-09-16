fc3c5eca81043f144a501e12ab25fec3209e93a7
dhardesty <dhardesty100@gmail.com>
2022-09-16 11:13:19 -0600

    days of coding

---
+++ day 5/introduction.js
@@ -0,0 +1,23 @@
+/*
+numbers 
+strings
+booleans
+null
+undefined
+
+arrays
+objects/dictionaries
+
+var firstName = "Alex";
+let lastName = "Miller";
+const bootcamp = "Coding Dojo"
+let age = 20;
+let areYouMarried = false;
+let grade; 
+let currentStack = null;
+
+firstName = "Alexander";
+*/
+
+let result = (4 * 5) - 10 / 2 +( 20 % 3 );
+console.log(result);
