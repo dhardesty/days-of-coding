33678c200c1cf19a7a769e88a3d96bd9c4e57e1d
dhardesty <dhardesty100@gmail.com>
2022-09-20 22:13:05 -0600

    days of coding

--- Likes/script.js
+++ Likes/script.js
@@ -1,6 +1,6 @@
 
 function liked(element){
-    var bleh = parseInt(element.parentElement.querySelector(".amountoflikes").innerText);
+    var content = parseInt(element.parentElement.querySelector(".amountoflikes").innerText);
     var placeholder = element.parentElement.querySelector(".amountoflikes");
-    placeholder.innerText = bleh + 1;
+    placeholder.innerText = content + 1;
 }
